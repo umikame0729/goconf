a8459224002f11e95f83d8c6f86f202aa72b6431
UMiKaMe0729 <shangtangcct+github@gmail.com>
2022-08-14 22:29:36 +0800

    feat: add different error

--- goconf.go
+++ goconf.go
@@ -7,6 +7,33 @@ import (
 	"time"
 )
 
+type ErrorIsUpdated struct {
+	Path    string
+	BakPath string
+}
+
+func (e *ErrorIsUpdated) Error() string {
+	return fmt.Sprintf("`%s` updated, backup at `%s`", e.Path, e.BakPath)
+}
+
+func IsUpdated(err error) bool {
+	_, ok := err.(*ErrorIsUpdated)
+	return ok
+}
+
+type ErrorIsNewCreated struct {
+	Err error
+}
+
+func (e *ErrorIsNewCreated) Error() string {
+	return e.Err.Error()
+}
+
+func IsNewCreated(err error) bool {
+	_, ok := err.(*ErrorIsNewCreated)
+	return ok
+}
+
 // Config definition
 type Config[T interface{}] struct {
 	is_load bool
@@ -30,21 +57,21 @@ func (c *Config[T]) Save() error {
 	return nil
 }
 
-func (c *Config[T]) IsExist() error {
+func (c *Config[T]) IsExist() bool {
 	if _, err := os.Stat(c.Path); os.IsExist(err) {
-		return nil
+		return true
 	} else {
-		return err
+		return false
 	}
 }
 
 func (c *Config[T]) createIfNotExist() error {
-	if err := c.IsExist(); err == nil {
+	if c.IsExist() {
 		return nil
 	} else if err := c.Save(); err != nil {
 		return err
 	}
-	return nil
+	return &ErrorIsNewCreated{}
 }
 
 func (c *Config[T]) bakName() string {
@@ -52,35 +79,41 @@ func (c *Config[T]) bakName() string {
 	return fmt.Sprintf("%s.%d-%d-%d-%d-%d-%d", c.Path, now.Local().Year(), now.Local().Month(), now.Local().Day(), now.Local().Hour(), now.Local().Minute(), now.Local().Second())
 }
 
-func (c *Config[T]) versionCheck() (bool, error) {
+func (c *Config[T]) versionCheck() error {
+	bakPath := c.bakName()
 	if c.Content.Version == c.Version {
-		return true, nil
-	} else if err := c.IsExist(); err != nil {
-		return false, err
-	} else if err := os.Rename(c.Path, c.bakName()); err != nil {
-		return false, err
+		return nil
+	} else if !c.IsExist() {
+		return fmt.Errorf("`%s` not exist", c.Path)
+	} else if err := os.Rename(c.Path, bakPath); err != nil {
+		return err
 	} else if err := c.Save(); err != nil {
-		return false, err
+		return err
 	}
-	return false, nil
+	return &ErrorIsUpdated{
+		Path:    c.Path,
+		BakPath: bakPath,
+	}
 }
 
 // bool - is version checked OK, if false, need rerun program
-func (c *Config[T]) Load() (bool, error) {
-	c.Content.Data = c.Data
-	if c.is_load {
-		return true, nil
-	} else if err := c.createIfNotExist(); err != nil {
-		return false, err
-	} else if data, err := os.ReadFile(c.Path); err != nil {
-		return false, err
-	} else if err := json.Unmarshal(data, &c.Content); err != nil {
-		return false, err
-	} else if update, err := c.versionCheck(); err != nil {
-		return false, err
-	} else if !update {
-		return false, nil
+func (c *Config[T]) Load() error {
+	if c.Content.Data == nil {
+		c.Content.Data = c.Data
 	}
+
+	if c.is_load {
+		return nil
+	} else if err := c.createIfNotExist(); err != nil {
+		return err
+	} else if data, err := os.ReadFile(c.Path); err != nil {
+		return err
+	} else if err := json.Unmarshal(data, &c.Content); err != nil {
+		return err
+	} else if err := c.versionCheck(); err != nil {
+		return err
+	}
+
 	c.is_load = true
-	return true, nil
+	return nil
 }
